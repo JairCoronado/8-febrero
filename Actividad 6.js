597e2b23febc3caf38d3cfcad83b4e99abb3362e
JairTigreRobotoics <47120897+JairTigreRobotoics@users.noreply.github.com>
2019-02-16 20:33:18 -0600

    Add files via upload

---
+++ Actividad 6.js
@@ -0,0 +1,43 @@
+int diceSize = 117;
+
+void setup() {
+  frameRate(10);
+  size(diceSize*3, diceSize*2);
+  noLoop();
+}
+
+void draw() {
+  background(255);
+
+  //dice
+  noStroke();
+  fill(0,255,0);
+  rectMode(CENTER);
+  rect(width/2, height/2, diceSize, diceSize, diceSize/5);
+
+  //dots
+  fill(50);
+  int side = int(random(1, 7));
+  if (side == 1 || side == 3 || side == 5)
+    ellipse(width/2, height/2, diceSize/5, diceSize/5); 
+  if (side == 2 || side == 3 || side == 4 || side == 5 || side == 6) { 
+    ellipse(width/2 - diceSize/4, height/2 - diceSize/4, diceSize/5, diceSize/5);
+    ellipse(width/2 + diceSize/4, height/2 + diceSize/4, diceSize/5, diceSize/5);
+  }
+  if (side == 4 || side == 5 || side == 6) {
+    ellipse(width/2 - diceSize/4, height/2 + diceSize/4, diceSize/5, diceSize/5);
+    ellipse(width/2 + diceSize/4, height/2 - diceSize/4, diceSize/5, diceSize/5);
+  }
+  if (side == 6) {
+    ellipse(width/2, height/2 - diceSize/4, diceSize/5, diceSize/5);
+    ellipse(width/2, height/2 + diceSize/4, diceSize/5, diceSize/5);
+  }
+
+  //roll
+  if (mousePressed && mouseButton == LEFT)
+    noLoop();
+}
+
+void mousePressed() {
+  loop();
+}
